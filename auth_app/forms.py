ead87b44693fbbd1435a00c2138d92aab4484e94
Muhammedshibil-123 <127181540+Muhammedshibil-123@users.noreply.github.com>
2025-11-04 23:27:50 +0530

    fr

--- auth_app/forms.py
+++ auth_app/forms.py
@@ -20,10 +20,11 @@ class MentorRegistrationForm(UserCreationForm):
 
     department=forms.ModelChoiceField(queryset=Department.objects.all(),required=True ,empty_label="Select Department")
     course=forms.ModelChoiceField(queryset=Course.objects.all(),required=True,empty_label="Select Course")
-    year_of_semester=forms.CharField(max_length=20,required=False)
+    year=forms.CharField(max_length=20,required=False)
     division_batch=forms.CharField(max_length=20,required=False)
-    contact_number=forms.CharField(max_length=12,required=False)
-    date_of_joining=forms.ImageField(required=False)
+    mentor_code=forms.CharField(max_length=10,required=False)
+    contact_number=forms.CharField(max_length=20,required=False)
+    date_of_joining=forms.DateField(required=False)
 
 
     class Meta(UserCreationForm.Meta):
@@ -40,8 +41,9 @@ class MentorRegistrationForm(UserCreationForm):
             profile_data = {
             'department': self.cleaned_data.get('department'),
             'course': self.cleaned_data.get('course'),
-            'year_semester': self.cleaned_data.get('year_semester'),
+            'year': self.cleaned_data.get('year'),
             'division_batch': self.cleaned_data.get('division_batch'),
+            'mentor_code':self.cleaned_data.get('mentor_code'),
             'contact_number': self.cleaned_data.get('contact_number'),
             'date_of_joining': self.cleaned_data.get('date_of_joining'),
             'profile_picture': self.cleaned_data.get('profile_picture'),
--- mentor/models.py
+++ mentor/models.py
@@ -9,20 +9,21 @@ class Department(models.Model):
     
 class Course(models.Model):
     name=models.CharField(max_length=100)
-    Department=models.ForeignKey(Department,on_delete=models.CASCADE)
+    department=models.ForeignKey(Department,on_delete=models.CASCADE)
     def __str__(self):
-        return f"{self.name} ({self.Department.name})"
+        return f"{self.name} ({self.department.name})"
 
 class MentorProfile(models.Model):
     user=models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='mentor_profile')
 
     department=models.ForeignKey(Department,on_delete=models.SET_NULL,null=True,blank=True)
-    course=models.ForeignKey(Department,on_delete=models.SET_NULL,null=True,blank=True)
-    year_semester=models.CharField(max_length=20,blank=True,null=True)
+    course=models.ForeignKey(Course,on_delete=models.SET_NULL,null=True,blank=True)
+    year=models.CharField(max_length=20,blank=True,null=True)
     division_batch=models.CharField(max_length=20,blank=True,null=True)
     contact_number=models.CharField(max_length=20,blank=True,null=True)
     date_of_joining=models.DateField(null=True,blank=True)
-    profile_picture=models.models.ImageField( upload_to='porfile_pics/mentors/',null=True,blank=True)
+    mentor_code=models.CharField(max_length=10,blank=True,null=True)
+    profile_picture=models.ImageField( upload_to='porfile_pics/mentors/',null=True,blank=True)
 
     def __str__(self):
         return f"profile of {self.user.username}"
