e870ba6fb3b4e44dd7403b2ce7dcb9c9e8fe56e0
Muhammedshibil-123 <127181540+Muhammedshibil-123@users.noreply.github.com>
2025-11-04 16:28:15 +0530

    f

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/',views.landing_page,name='register'),
+    path('',views.landing_page,name='register'),
     path('login/',views.login_view,name='login'),
     path('logout/',views.logout_view,name='logout'),
-    path('regiter/student/',views.student_register,name='student_register'),
+    path('register/student/',views.student_register,name='student_register'),
     path('register/mentor/',views.mentor_register,name='mentor_register')
 ]
